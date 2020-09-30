33202e640586cf628a2ad360e3608be9d9c89754
prestonwin78 <prestonwin78@gmail.com>
2020-09-30 13:04:50 -0500

    Change landing design

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import "./styles/styles.css";
 function Header() {
   return (
     <div className="header">
-      <h3 id="title">Scorebar</h3>
+      <h3 id="title">Goalbug</h3>
     </div>
   );
 }
--- src/Landing.js
+++ src/Landing.js
@@ -5,15 +5,34 @@ function Landing(props) {
   return (
     <div className="body">
       <div className="card">
-        <h1 className="landing-header">Scorebar - Designed for Twitch/OBS</h1>
-        <p className="landing-paragraph">
-          With Scorebar, you can design a custom, modern scorebar to suit your
-          needs and improve your stream.
-        </p>
+        <h1 className="landing-header">Goalbug</h1>
+        <div className="info-container">
+          <h2>Modern Scoreboard for Streams</h2>
+          <p>
+            With Goalbug, you can design a custom, modern scorebar to fit your
+            needs and improve your stream.
+          </p>
+          <ul>
+            <li>Customize a scoreboard's color and text</li>
+            <li>
+              Choose how the scoreboard behaves when adding and subtracting
+              points
+            </li>
+            <li>Easy to use</li>
+            <li>Works well with OBS</li>
+          </ul>
+        </div>
       </div>
       <div className="image-container">
         <img
           src={require("./Images/example.PNG")}
+          alt="example scorebar"
+          className="scorebar-example"
+        ></img>
+      </div>
+      <div className="image-container">
+        <img
+          src={require("./Images/settings.png")}
           alt="example scorebar"
           className="scorebar-example"
         ></img>
